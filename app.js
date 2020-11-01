d8c25be246d6de52ee84de0bc123338b438984a0
Alex Kahler <kahler197@gmail.com>
2020-11-01 11:45:56 -0600

    gitignore

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
-const fs = require('fs');
+const inquirer = require('inquirer');
+//const fs = require('fs');
+//const generatePage = require('./src/page-template');
 
-const generatePage = require('./src/page-template');
+//const pageHTML = generatePage(name, github);
 
-const profileDataArgs = process.argv.slice(2);
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
+
+
+//fs.writeFile('./index.html', pageHTML, err => {
+    //if (err) throw err;
+  
+    //console.log('Portfolio complete! Check out index.html to see the output!');
+  //});
+
+
+
+
+// NOTES
+
+//const profileDataArgs = process.argv.slice(2);
 //console.log(profileDataArgs);
 
 // displays strings in line txt
@@ -15,12 +39,4 @@ const profileDataArgs = process.argv.slice(2);
 // const github = profileDataArgs[1];
 
 // destructered elements into one 
-const [name, github] = profileDataArgs;
-
-
-
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
-  
-    console.log('Portfolio complete! Check out index.html to see the output!');
-  });
+//const [name, github] = profileDataArgs;
